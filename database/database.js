4813392366171901a8039f405b91417977bb0a91
Elkin Urango <Elkin@Elkins-iMac.local>
2016-02-15 01:14:24 -0500

    Add mongolab

--- database/database.js
+++ database/database.js
@@ -3,8 +3,8 @@
 var mongoose = require('mongoose');
 
 exports.connect = function(err, res){
-	mongoose.connect('mongodb://localhost/lf_db');// LOCAL
-	//mongoose.connect('mongodb://root:root@ds051873.mongolab.com:51873/forshare_db');// REMOTE
+	// mongoose.connect('mongodb://localhost/lf_db');// LOCAL
+	mongoose.connect('mongodb://root:root@ds051873.mongolab.com:51873/forshare_db');// REMOTE
 
 	if (err) throw err;
 	console.log('CONNECT TO DABASE');
